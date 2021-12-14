a2e579a65c1aa44e80fe1e636b8e49d55bc5bb0d
buddhadeb33 <buddhadeb33@gmail.com>
2021-12-14 12:30:21 +0530

    new

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,9 +9,9 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Buddhadeb Mondal </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            from <span className="purple"> West Bengal, India.</span>
+            currently living in <span className="purple">  Bangalore, India.</span>
+            <br />I am a Senior Software Engineer working in L&T Technology Services.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -29,9 +29,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            “The most common way people give up their power is by thinking they don't have any.”{" "}
           </p>
-          <footer className="blockquote-footer">Buddhadeb</footer>
+          <footer className="blockquote-footer">— Alice Walker.</footer>
         </blockquote>
       </Card.Body>
     </Card>
